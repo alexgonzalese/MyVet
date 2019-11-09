5d100b32116d30e6d6be12690a02bdb7492e53c8
ALEXINMUSER <alexgonzalese@gmail.com>
2019-11-09 14:55:34 -0500

    New Seead

--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyVet.Web.Data;
+using MyVet.Web.Data.Entities;
+using MyVet.Web.Helpers;
 
 namespace MyVet.Web
 {
@@ -28,10 +31,23 @@ namespace MyVet.Web
     {
       services.Configure<CookiePolicyOptions>(options =>
       {
-              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-              options.CheckConsentNeeded = context => true;
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
+
+      //configuracion de la clave de usuario
+      services.AddIdentity<User, IdentityRole>(cfg =>
+        {
+          cfg.User.RequireUniqueEmail = true;
+          cfg.Password.RequireDigit = true;
+          cfg.Password.RequiredUniqueChars = 0;
+          cfg.Password.RequireLowercase = true;
+          cfg.Password.RequireNonAlphanumeric = true;
+          cfg.Password.RequireUppercase = false;
+
+        })
+        .AddEntityFrameworkStores<DataContext>();
 
       services.AddDbContext<DataContext>(cfg =>
       {
@@ -39,6 +55,7 @@ namespace MyVet.Web
       });
 
       services.AddTransient<SeedDb>();
+      services.AddScoped<IUserHelper, UserHelper>();
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
@@ -57,6 +74,7 @@ namespace MyVet.Web
 
       app.UseHttpsRedirection();
       app.UseStaticFiles();
+      app.UseAuthentication();
       app.UseCookiePolicy();
 
       app.UseMvc(routes =>
